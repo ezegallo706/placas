ceacd88c9a81e0649956b2eb43c3e8711fd3d8d1
CristianHourcade <hourcadecristian@gmail.com>
2018-02-19 19:27:38 -0300

    Bootstrap componenetes instalados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -16,7 +16,8 @@ import { NavbarComponent } from './navbar/navbar.component';
     NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot ()
   ],
   providers: [],
   bootstrap: [AppComponent]
